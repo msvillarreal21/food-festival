758861959c9044bbb5f92c16b7a99dcf8f07f76b
mariasalomevillarreal <msvillarreal2021@gmail.com>
2022-01-30 15:23:26 -0600

    adding webpack pwa manifest

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const webpack = require('webpack');
 const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 // const SWPrecacheWebpackPlugin = require("sw-precache-webpack-plugin");
-// const WebpackPwaManifest = require("webpack-pwa-manifest");
+const WebpackPwaManifest = require("webpack-pwa-manifest");
 const path = require('path');
 
 const config = {
